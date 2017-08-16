d7f4042cf5562ee4ae8b34f1f9bf7220172773ca
jhempsey <justin.hempsey@badunetworks.com>
2017-08-15 19:42:37 -0700

    Added git-radar as a submodule so that it will be installed with dotfiles

--- install.sh
+++ install.sh
@@ -8,10 +8,12 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=(bashrc screenrc bash_aliases bash_profile vimrc vim tmux.conf) #            # list of files/folders to symlink in homedir
+files=(bashrc screenrc bash_aliases bash_profile vimrc vim tmux.conf git-radar) #            # list of files/folders to symlink in homedir
 
 ##########
 
+# Update submodules
+git submodule update --init --recursive
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
